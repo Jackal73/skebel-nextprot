d19f4270400e351ee589e219f621f0be3641e260
Shawn Kebel <fxrender@outlook.com>
2024-05-18 14:55:33 -0500

    :boom::sparkles::dizzy: Add `Hero` and `FloatingNav` components for `improved landing page layout`

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1 className="">Hello Portfolio</h1>
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
+        <Hero />
       </div>
     </main>
   );
